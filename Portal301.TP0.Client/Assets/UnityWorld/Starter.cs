3d759eadcbbd8bdec2e46e196a8fc9ff96fcd616
cn0 <st.kaldalis@gmail.com>
2025-05-01 10:03:43 +0900

    MainScene 추가

--- Portal301.TP0.Client/Assets/UnityWorld/Starter.cs
+++ Portal301.TP0.Client/Assets/UnityWorld/Starter.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections;
+﻿using Portal301.TP0.Client.Core;
+using Portal301.TP0.Client.UnityWorld.View;
+using System.Collections;
 using UnityEngine;
 
 namespace Portal301.TP0.Client.UnityWorld
 {
     public class Starter : MonoBehaviour
     {
+        [SerializeField]
+        private MainScene mainScene;
+
         // Use this for initialization
         void Start()
         {
-
+            Destroy(gameObject);
         }
 
         // Update is called once per frame
---
+++ Portal301.TP0.Client/Assets/UnityWorld/View/MainScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Portal301.TP0.Client.UnityWorld.View
+{
+    public class MainScene : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
